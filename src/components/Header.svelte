6dcc3e3a7b65ae957ccae21ae3db41b492b62921
Andreas Lauhard <andreas.lauhard@bsoftware.at>
2020-12-14 21:07:31 +0100

    add header component

---
+++ src/components/Header.svelte
@@ -0,0 +1,26 @@
+<!-- script -->
+<script>
+    export let attributes = {};
+    $: attributes = attributes;
+</script>
+<!-- html -->
+<svelte:head>
+    <!-- head content -->
+</svelte:head>
+<header>
+    <h1>{attributes.title}</h1>
+    <p>by: {attributes.author}</p>
+    <span>Creation date: {attributes.creationDate} </span>
+    
+</header>
+<!-- css -->
+<style>
+    header{
+        background-color: blueviolet;
+        padding: 1em;
+    }
+    p {
+        margin: 0;
+        padding: 0;
+    }
+</style>
--- src/routes/blog/_layout_blog.svelte
+++ src/routes/blog/_layout_blog.svelte
@@ -1,11 +1,11 @@
 <script>
     import { stores } from '@sapper/app';
     import { onMount } from 'svelte';
-import { attr } from 'svelte/internal';
     const { page, preloading } = stores();
-    
+    import Header from '../../components/Header.svelte';
+
     let path = $page.path;
-    export let attributes;
+    export let attributes = {};
     export let tags = [];
     onMount (async () => {
         attributes = await fetch(`${path}.json`).then(r => r.json());
@@ -26,11 +26,11 @@ import { attr } from 'svelte/internal';
 	}
 </style>
 
-
 <main>
     {#each tags as tag}
         {tag}
     {/each}
+    <Header {attributes} ></Header>
     <!-- blogpost layout -->
 	<slot></slot>
 </main>
