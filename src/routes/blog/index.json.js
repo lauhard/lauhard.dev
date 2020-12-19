133aa7255af79db9d8698d61ac83dabb1474074f
AndyCandy <56514575+lauhard@users.noreply.github.com>
2020-12-20 00:29:48 +0100

    try fix page store problem

--- src/routes/blog/index.json.js
+++ src/routes/blog/index.json.js
@@ -1,34 +1,16 @@
 import posts from './_posts.js';
 
-// const contents = JSON.stringify(posts.map(post => {
-// 	return {
-// 		title: post.title,
-//         slug: post.slug,
-//         creationDate: post.creationDate,
-//         tags: post.tags,
-//         author: post.author
-// 	};
-// }));
+const contents = JSON.stringify(posts.map(post => {
+	return {
+		title: post.title,
+        slug: post.slug,
+        creationDate: post.creationDate,
+        tags: post.tags,
+        author: post.author
+	};
+}));
 
 export function get(req, res) {
-	console.log(req.path)
-	let slug = req.path.replace(".json","").replace("/","");
-	console.log(slug)
-
-	let filteredPosts = posts.filter(p => p.slug == slug);
-	if (filteredPosts.length == 0)
-		filteredPosts = posts;
-	console.log(filteredPosts);
-	const contents = JSON.stringify(filteredPosts.map(post => {
-		return {
-			title: post.title,
-			slug: post.slug,
-			creationDate: post.creationDate,
-			tags: post.tags,
-			author: post.author
-		};
-	}));
-	
 	res.writeHead(200, {
 		'Content-Type': 'application/json'
 	});
--- src/routes/blog/test/index.json.js
+++ src/routes/blog/test/index.json.js
@@ -1,4 +1,4 @@
-import posts from './_posts.js';
+import posts from '../_posts.js';
 
 const lookup = new Map();
 posts.forEach(post => {
@@ -8,14 +8,14 @@ posts.forEach(post => {
 export function get(req, res, next) {
 	// the `slug` parameter is available because
 	// this file is called [slug].json.js
-	const { slug } = req.params;
-
-	if (lookup.has(slug)) {
+	let s = req.path.split('/')[2].replace(".json","");
+	console.log(s);
+	if (lookup.has(s)) {
 		res.writeHead(200, {
 			'Content-Type': 'application/json'
 		});
 
-		res.end(lookup.get(slug));
+		res.end(lookup.get(s));
 	} else {
 		res.writeHead(404, {
 			'Content-Type': 'application/json'
