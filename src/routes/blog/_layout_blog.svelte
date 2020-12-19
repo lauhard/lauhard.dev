9dad4c6a0b415333d12dc894525cde654a07c87a
AndyCandy <56514575+lauhard@users.noreply.github.com>
2020-12-19 22:52:54 +0100

    try fix page store problem

--- src/routes/blog/_layout_blog.svelte
+++ src/routes/blog/_layout_blog.svelte
@@ -18,7 +18,7 @@
                 fixedPath = fixedPath.slice(0, -1);
             }
             console.log("try fetch attributes...") 
-            attributes = await fetch(`blog/test.json`).then(r => r.json());
+            attributes = await fetch(`/blog/test.json`).then(r => r.json());
             console.log("attributes", attributes);
             if ($Posts.length == 0) {
                 console.log("try fetch posts again...") 
